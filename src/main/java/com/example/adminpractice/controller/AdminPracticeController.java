6a7914a9eb167a8499184bc6f5761e9397b70a24
Hakjong Son <hakjong.son@gmail.com>
2020-02-17 18:50:14 +0900

    Init

---
+++ src/main/java/com/example/adminpractice/controller/AdminPracticeController.java
@@ -0,0 +1,13 @@
+package com.example.adminpractice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminPracticeController {
+
+    @GetMapping(path = "/")
+    public String index() {
+        return "index";
+    }
+}
