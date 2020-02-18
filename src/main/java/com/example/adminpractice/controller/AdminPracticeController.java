c39e8e43bffbc04ea71a2e2367596a9a5a49b405
Hakjong Son <hakjong.son@gmail.com>
2020-02-18 15:21:45 +0900

    login (in memory user)

--- src/main/java/com/example/adminpractice/controller/AdminPracticeController.java
+++ src/main/java/com/example/adminpractice/controller/AdminPracticeController.java
@@ -1,7 +1,14 @@
 package com.example.adminpractice.controller;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class AdminPracticeController {
@@ -9,5 +16,26 @@ public class AdminPracticeController {
     @GetMapping(path = "/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping(path = "/login")
+    public String login() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (!(auth instanceof AnonymousAuthenticationToken)) {
+            return "redirect:/";
+        }
+
+        return "login";
+    }
+
+    @GetMapping(path = "/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler()
+                    .logout(request, response, auth);
+        }
+        return "redirect:/login";
     }
 }
